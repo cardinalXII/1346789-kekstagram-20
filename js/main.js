0b441ea7d8ff2e4565f106f8e3af5595477f6455
Evgeniy <karatikus22@gmail.com>
2020-06-15 17:06:44 +0300

    исправил замечания

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 'use strict';
-var WORDS_COMMENTS = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра.', 'В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
-var NAMES_COMENTS = ['Кекс', 'Евгений', 'Ульяна', 'Сергей', 'Боб', 'Владыка', 'Кришна', 'Вишну', 'Хануман', 'Гонеш', 'Сварог', 'Велес', 'Ярослав'];
-var PICTURE_TEMPLATEN = document.querySelector('#picture').content.querySelector('.picture');
+var COMMENTS = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра.', 'В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
+var NAMES = ['Кекс', 'Евгений', 'Ульяна', 'Сергей', 'Боб', 'Владыка', 'Кришна', 'Вишну', 'Хануман', 'Гонеш', 'Сварог', 'Велес', 'Ярослав'];
+var PICTURE_TEMPLATE = document.querySelector('#picture').content.querySelector('.picture');
 var PICTURES_CONTAINER = document.querySelector('.pictures');
-
+var MAX_NUMBER = 26;
 
 // случайное число от и до
 var randomInteger = function (min, max) {
@@ -13,23 +13,23 @@ var randomInteger = function (min, max) {
 
 // генерация коментариев
 var messages = [];
-for (var l = 0; l < 25; l++) {
-  var randomIndexWord = Math.floor(Math.random() * WORDS_COMMENTS.length);
-  var message = WORDS_COMMENTS[randomIndexWord];
+for (var l = 0; l < MAX_NUMBER; l++) {
+  var randomIndexWord = Math.floor(Math.random() * COMMENTS.length);
+  var message = COMMENTS[randomIndexWord];
   messages.push(message);
 }
 
 // генерация имени
 var names = [];
-for (var n = 0; n < 25; n++) {
-  var randomIndexName = Math.floor(Math.random() * NAMES_COMENTS.length);
-  var name = NAMES_COMENTS[randomIndexName];
-  names.push(name);
+for (var n = 0; n < MAX_NUMBER; n++) {
+  var randomIndexName = Math.floor(Math.random() * NAMES.length);
+  var nameUser = NAMES[randomIndexName];
+  names.push(nameUser);
 }
 
 // создание коментраиев
 var generatorComments = function () {
-  var num = randomInteger(3, 25);
+  var num = randomInteger(3, MAX_NUMBER);
   var comments = [];
   for (var c = 0; c < num; c++) {
     comments.push({
@@ -43,7 +43,7 @@ var generatorComments = function () {
 
 // массив вывода фото
 var photos = [];
-for (var i = 1; i < 26; i++) {
+for (var i = 1; i < MAX_NUMBER; i++) {
   photos.push({
     url: 'photos/' + i + '.jpg',
     description: 'Класная фотка',
@@ -52,7 +52,7 @@ for (var i = 1; i < 26; i++) {
   });
 }
 var renderPicture = function (photo) {
-  var pictureElement = PICTURE_TEMPLATEN.cloneNode(true);
+  var pictureElement = PICTURE_TEMPLATE.cloneNode(true);
 
   pictureElement.querySelector('img').setAttribute('src', photo.url);
   pictureElement.querySelector('img').setAttribute('alt', photo.description);
