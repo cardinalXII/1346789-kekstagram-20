6aefe7e4ffaaca58b22038e2d40f493b4e81b3d9
Evgeniy <karatikus22@gmail.com>
2020-06-15 16:28:48 +0300

    исправил замечания

--- js/main.js
+++ js/main.js
@@ -1,72 +1,39 @@
 'use strict';
 var WORDS_COMMENTS = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра.', 'В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
 var NAMES_COMENTS = ['Кекс', 'Евгений', 'Ульяна', 'Сергей', 'Боб', 'Владыка', 'Кришна', 'Вишну', 'Хануман', 'Гонеш', 'Сварог', 'Велес', 'Ярослав'];
-var USERS_ORIGIN = document.querySelector('#picture').content.querySelector('.picture');
-var USERS_CLONE = document.querySelector('.pictures');
+var PICTURE_TEMPLATEN = document.querySelector('#picture').content.querySelector('.picture');
+var PICTURES_CONTAINER = document.querySelector('.pictures');
 
 
-// число от 1 до 6 для аватара
-var avatars = [];
-for (var t = 0; t < 25; t++) {
-  var randomInteger = function (min, max) {
-    // случайное число от min до (max+1)
-    var randava = min + Math.random() * (max + 1 - min);
-    return Math.floor(randava);
-  };
-  var randavas = randomInteger(1, 6);
-  avatars.push(randavas);
-}
-
-// настройки лайков
-var likes = [];
-for (var j = 0; j < 25; j++) {
-  var randomIntegerLikes = function (min, max) {
-    // случайное число от min до (max+1)
-    var rand = min + Math.random() * (max + 1 - min);
-    return Math.floor(rand);
-  };
-  var like = randomIntegerLikes(15, 200);
-  likes.push(like);
-}
-
-// число от 1 до 25 для картинок
-var numbers = [];
-for (var k = 1; k < 26; k++) {
-  var randNum = k;
-  numbers.push(randNum);
-}
+// случайное число от и до
+var randomInteger = function (min, max) {
+  var randomNumber = min + Math.random() * (max + 1 - min);
+  return Math.floor(randomNumber);
+};
 
 // генерация коментариев
 var messages = [];
 for (var l = 0; l < 25; l++) {
   var randomIndexWord = Math.floor(Math.random() * WORDS_COMMENTS.length);
-  var mess = WORDS_COMMENTS[randomIndexWord];
-  messages.push(mess);
+  var message = WORDS_COMMENTS[randomIndexWord];
+  messages.push(message);
 }
 
 // генерация имени
 var names = [];
 for (var n = 0; n < 25; n++) {
   var randomIndexName = Math.floor(Math.random() * NAMES_COMENTS.length);
-  var nameIndex = NAMES_COMENTS[randomIndexName];
-  names.push(nameIndex);
+  var name = NAMES_COMENTS[randomIndexName];
+  names.push(name);
 }
 
-// настройки вывода коментариев
-// генерируем случайное число комментариев
-var randomIntegerComments = function (min, max) {
-  var rand = min + Math.random() * (max + 1 - min);
-  return Math.floor(rand);
-};
 // создание коментраиев
-var generationComments = function () {
-
-  var num = randomIntegerComments(3, 25);
-
+var generatorComments = function () {
+  var num = randomInteger(3, 25);
   var comments = [];
   for (var c = 0; c < num; c++) {
     comments.push({
-      avatar: 'img/avatar-' + avatars[c] + '.svg',
+      avatar: 'img/avatar-' + randomInteger(1, 6) + '.svg',
       message: messages[c],
       name: names[c]
     });
@@ -74,25 +41,25 @@ var generationComments = function () {
   return comments;
 };
 
-// массив настройки вывода фото
+// массив вывода фото
 var photos = [];
-for (var i = 0; i < 25; i++) {
+for (var i = 1; i < 26; i++) {
   photos.push({
-    url: 'photos/' + numbers[i] + '.jpg',
+    url: 'photos/' + i + '.jpg',
     description: 'Класная фотка',
-    likes: likes[i],
-    comments: generationComments()
+    likes: randomInteger(15, 200),
+    comments: generatorComments()
   });
 }
 var renderPicture = function (photo) {
-  var userElement = USERS_ORIGIN.cloneNode(true);
+  var pictureElement = PICTURE_TEMPLATEN.cloneNode(true);
 
-  userElement.querySelector('img').setAttribute('src', photo.url);
-  userElement.querySelector('img').setAttribute('alt', photo.description);
-  userElement.querySelector('.picture__likes').textContent = photo.likes;
-  userElement.querySelector('.picture__comments').textContent = photo.comments.length;
+  pictureElement.querySelector('img').setAttribute('src', photo.url);
+  pictureElement.querySelector('img').setAttribute('alt', photo.description);
+  pictureElement.querySelector('.picture__likes').textContent = photo.likes;
+  pictureElement.querySelector('.picture__comments').textContent = photo.comments.length;
 
-  return userElement;
+  return pictureElement;
 };
 
 var fragment = document.createDocumentFragment();
@@ -101,4 +68,4 @@ for (var g = 0; g < photos.length; g++) {
   fragment.appendChild(renderPicture(photos[g]));
 }
 
-USERS_CLONE.appendChild(fragment);
+PICTURES_CONTAINER.appendChild(fragment);
